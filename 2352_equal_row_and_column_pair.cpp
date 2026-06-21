b2ea2429b5e6afba170284c73e768698eb5bcf7c
PrabhdeepKaur12 <prabhkalate@gmail.com>
2026-06-21 22:18:56 +0530

    Add files via upload

---
+++ 2352_equal_row_and_column_pair.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+         int n = grid.size();
+        map<vector<int>, int> rowCount;
+
+        for (int i = 0; i < n; i++) {
+            rowCount[grid[i]]++;
+        }
+
+        int ans = 0;
+
+        for (int col = 0; col < n; col++) {
+            vector<int> column;
+
+            for (int row = 0; row < n; row++) {
+                column.push_back(grid[row][col]);
+            }
+
+            ans += rowCount[column];
+        }
+
+        return ans;
+    }
+};
